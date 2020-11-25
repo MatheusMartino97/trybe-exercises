0fbf5a36a79aece57ac2f227d0b4c8df88b36efd
Matheus Martino do Amaral <matheus.martino97@gmail.com>
2020-11-25 20:54:40 -0300

    Adding a better description to the problem

--- modulo_1/bloco_4/dia_1/program_11.js
+++ modulo_1/bloco_4/dia_1/program_11.js
@@ -25,7 +25,7 @@ R$ 142,00 parcela a se deduzir do imposto.
 Fazendo a conta, temos: (7,5% de R$ 2.670,00) - R$ 142,80 = R$ 57,45
 O último cálculo para conseguir o salário líquido é R$ 2.670,00 - R$ 57,45 (salário-base - valor IR) = R$ 2.612,55.
 Resultado: R$ 2.612,55.
- */
+*/
 
 function calcSalarioLiquido(bruto) {
   let aliquotaINSS;
