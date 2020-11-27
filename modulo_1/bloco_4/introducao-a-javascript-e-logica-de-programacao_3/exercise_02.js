4d523d72b441bf1c40ab815c6ea73e8432a0c97a
Matheus Martino <matheus.martino97@gmail.com>
2020-11-27 16:48:33 -0300

    Resolvendo o terceiro exercicio

--- modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_3/exercise_02.js
+++ modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_3/exercise_02.js
@@ -1,4 +1,4 @@
-// Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos
+// 2 - Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos
 // de base.
 
 function triangle (n) {
