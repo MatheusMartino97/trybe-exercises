4d32fb7b14a9df5bb8d9e65c799b2ffceeb7aee7
Matheus Martino <matheus.martino97@gmail.com>
2020-11-27 17:08:05 -0300

    Resolvendo o quarto exercicio

---
+++ modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_3/exercise_04.js
@@ -0,0 +1,32 @@
+// 4 - Depois, faça uma pirâmide com n asteriscos de base
+
+function invertTriangle(n, char1, char2) {
+  let spaces;
+  let stars;
+  let line;
+
+  for (let i = 1; i <= n; i += 2) {
+    if (n % 2 === 0) {
+      spaces = char1;
+      stars = char2;
+    } else {
+      spaces = "";
+      stars = "";
+    }
+
+    for (let numOfSpaces = n - i; numOfSpaces > 0; numOfSpaces -= 2) {
+      spaces += char1;
+    }
+
+    for (let numOfStars = 1; numOfStars <= i; numOfStars++) {
+      stars += char2;
+    }
+    line = spaces + stars + spaces;
+    console.log(line);
+  }
+}
+
+invertTriangle(6, " ", "*");
+
+// Você pode escolher quais caracteres farão parte da pirâmide
+// Note que a função também funciona se a base for um número par
