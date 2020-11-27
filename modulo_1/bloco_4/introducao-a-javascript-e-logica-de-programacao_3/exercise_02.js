ec32569304ec42979ab2183d446a1b493a16af3e
Matheus Martino <matheus.martino97@gmail.com>
2020-11-27 15:44:27 -0300

    Resolvendo o segundo exercicio

---
+++ modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_3/exercise_02.js
@@ -0,0 +1,13 @@
+// Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos
+// de base.
+
+function triangle (n) {
+    let line = ''
+    
+    for (let i = 0; i < n; i++) {
+        line += '*'
+        console.log(line)
+    }
+}
+
+triangle (5)
