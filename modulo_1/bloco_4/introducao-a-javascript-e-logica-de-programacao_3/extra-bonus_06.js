d0405fb00d683f95c6ea795c2dba931f63a08aca
Matheus Martino <matheus.martino97@gmail.com>
2020-11-27 22:33:14 -0300

    Resolvendo o exerciocio bonus numero seis

--- modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_3/extra-bonus_06.js
+++ modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_3/extra-bonus_06.js
@@ -1,54 +1,19 @@
-// Bonus 06 - Faça uma pirâmide com n asteriscos de base que seja vazia no meio. Assuma que o valor de n
-// será sempre ímpar:
+// Bonus 06 - Faça um programa que diz se um número definido numa variável é primo ou não.
 
-function hollowPyramid(base, char1, char2) {
-    let n = Math.abs(base)
-    if (n % 2 ===0) {
-        n += 1
+function isPrime (n) {
+    let quantityOfDividers = 0
+
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) {
+            quantityOfDividers += 1
+        } 
+    }
+    if (quantityOfDividers === 0) {
+        console.log('É primo')
+    } else {
+        console.log('Não é primo')
     }
 
-  let side = "";
-  let middle = "";
-  let line;
-  let sideSize = (n - 1) / 2;
-
-  for (let i = 1; i <= n; i++) {
-    line = "";
-
-    if (i === 1) {
-      for (sideIndex = sideSize; sideIndex > 0; sideIndex--) {
-        side += char2;
-      }
-      line = side + char1 + side;
-      console.log(line);
-      middle += char2;
-      side = side.substr(0, side.length - 1);
-    }
-
-    if (i % 2 === 0) {
-      for (let lineIndex = 0; lineIndex < n; lineIndex++) {
-        line += char2;
-      }
-      console.log(line);
-    }
-
-    if (i % 2 !== 0 && i !== n && i !== 1) {
-      line = side + char1 + middle + char1 + side;
-      console.log(line);
-      middle += char2 + char2;
-      side = side.substr(0, side.length - 1);
-    }
-
-    if (i === n) {
-      for (let baseIndex = 0; baseIndex < n; baseIndex++) {
-        line += char1;
-      }
-      console.log(line);
-    }
-  }
 }
 
-hollowPyramid(5, "*", " ");
-
-// Note que você pode mudar o tamanho da arvore assim como os caracteres que a compoe
-// Note também que a função funciona mesmo com números negativos e números pares
+isPrime(5)
