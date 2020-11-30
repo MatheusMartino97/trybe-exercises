8fcac4c960f4718db8b5da6642578b245527ea62
Matheus Martino <matheus.martino97@gmail.com>
2020-11-30 19:29:09 -0300

    Formatando com Prettier

--- modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_4/parte_II-exercise_04.js
+++ modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_4/parte_II-exercise_04.js
@@ -1,14 +1,14 @@
 // 4 -  Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 
-function longestName (array) {
-    let currentLongest = array[0]
+function longestName(array) {
+  let currentLongest = array[0];
 
-    for (let index in array) {
-        if (array[index].length > currentLongest.length) {
-            currentLongest = array[index]
-        }
+  for (let index in array) {
+    if (array[index].length > currentLongest.length) {
+      currentLongest = array[index];
     }
-    console.log(currentLongest)
+  }
+  console.log(currentLongest);
 }
 
-longestName(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'])
+longestName(["José", "Lucas", "Nádia", "Fernanda", "Cairo", "Joana"]);
