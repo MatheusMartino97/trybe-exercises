d25dc7740ea927e98c875f87086cfadd5f0e9626
Matheus Martino <matheus.martino97@gmail.com>
2020-11-30 15:47:26 -0300

    Resolvendo segundo exercicio da parte 2

---
+++ modulo_1/bloco_4/introducao-a-javascript-e-logica-de-programacao_4/parte _II-exercises_02.js
@@ -0,0 +1,16 @@
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+function indexOfGreater (array) {
+    let greaterIndex
+    let currentGreater = array[0]
+
+    for (let index in array) {
+        if (array[index] > currentGreater) {
+            currentGreater = array[index]
+            greaterIndex = index
+        }
+    }
+    console.log(greaterIndex)
+}
+
+indexOfGreater([2, 3, 6, 7, 10, 1])
