81f02e99ffc5c3b52c084ab56bddb944a255dcab
Matheus Martino <matheus.martino97@gmail.com>
2020-12-03 22:26:27 -0300

    Finishing the challenges

--- modulo_1/bloco_5/javascript-dom-eventos-e-web-storage_1/script.js
+++ modulo_1/bloco_5/javascript-dom-eventos-e-web-storage_1/script.js
@@ -8,8 +8,18 @@
 // Crie uma função que corrija o texto da tag <h1>.
 // Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
 // Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+let p = document.querySelector('p')
 
-document.querySelector('p').innerText = 'Já trabalhando e ganhando o suficiente pra pagar a Trybe'
+p.innerText = 'Já trabalhando e ganhando o suficiente pra pagar a Trybe'
 document.getElementsByClassName('main-content')[0].style = 'background-color: rgb(76,164,109)'
 document.getElementsByClassName('center-content')[0].style = 'background-color: rgb(255,255,255)'
 document.querySelector('h1').innerText = 'Exercício 5.1 - JavaScript'
+
+let upperP = p.innerText.toUpperCase()
+
+p.innerText = upperP
+
+let paragraphs = document.querySelectorAll('p')
+for (let i of paragraphs) {
+    console.log(i.innerText)
+}
