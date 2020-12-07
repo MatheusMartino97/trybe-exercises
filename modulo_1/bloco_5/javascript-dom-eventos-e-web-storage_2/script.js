bf357016902d152eb619d15b83d5aa47e71b20da
Matheus Martino <matheus.martino97@gmail.com>
2020-12-07 16:49:32 -0300

    SOlving the second part

--- modulo_1/bloco_5/javascript-dom-eventos-e-web-storage_2/script.js
+++ modulo_1/bloco_5/javascript-dom-eventos-e-web-storage_2/script.js
@@ -56,3 +56,33 @@ for (let i = 1; i <= 3; i++) {
     divClassMainContent.appendChild(headingThree)
     headingThree.innerText = i +  '° h3'
 }
+
+//========================================================
+
+// 1 - Adicione a classe title na tag h1 criada;
+document.querySelector('h1').classList.add('title')
+let headingThreeList = document.querySelectorAll('h3')
+
+for (let heading of headingThreeList) {
+    heading.classList.add('description')
+}
+
+// 3 - Remova o div criado no passo 5 (aquele que possui a classe left-content).
+// document.querySelector('.left-content').parentElement.removeChild(document.querySelector('.left-content'))
+let leftContentParent = document.querySelector('.left-content').parentElement
+leftContentParent.removeChild(divClassLeftContent)
+
+// 4 - Centralize o div criado no passo 6 (aquele que possui a classe right-content).
+divClassRightContent.style.marginRight = 'auto'
+
+// 5 - Troque a cor de fundo do elemento pai da div criada no passo 3 (aquela que possui a classe center-content ) para a cor verde;
+let centerContentParent = document.querySelector('.center-content').parentElement
+centerContentParent.style.backgroundColor = 'green'
+
+// 6 - Remova os dois últimos elementos ( nove e dez ) da lista criada no passo 8.
+let listItemList = document.querySelectorAll('li')
+
+for (let i = 9; i <= 10; i++) {
+    unorderedList.removeChild(listItemList[i - 1])
+}
+console.log(listItemList)
