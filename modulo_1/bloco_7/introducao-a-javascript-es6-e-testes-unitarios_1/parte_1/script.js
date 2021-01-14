7866e0ad5e6476c4c44d4d3117af30803458b6f8
Matheus Martino <matheus.martino97@gmail.com>
2021-01-13 21:49:30 -0300

    Solving the first task of the second exercise.

--- modulo_1/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_1/parte_1/script.js
+++ modulo_1/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_1/parte_1/script.js
@@ -1,16 +1,20 @@
+
+// Faça as modificações necessárias na função para que o seu comportamento respeite o escopo no qual cada variável foi declarada.
 const testingScope = escopo => { 
     if (escopo === true) { 
-      var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      const ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
       console.log(ifScope);
     } else {
-      var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
       console.log(elseScope);
     }
     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
   }
 
   testingScope(true);
+
+  // Copie o código abaixo e faça uma função que retorne o array oddsAndEvens em ordem crescente.
 
   // const compare = (a, b) => a - b
 
