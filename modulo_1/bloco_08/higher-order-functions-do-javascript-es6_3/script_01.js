f5d6593eb0abf3503315c4c1e205be944a3a29d7
Matheus Martino <matheus.martino97@gmail.com>
2021-01-20 15:57:50 -0300

    Solving the exercise 2.

--- modulo_1/bloco_08/higher-order-functions-do-javascript-es6_3/script_01.js
+++ modulo_1/bloco_08/higher-order-functions-do-javascript-es6_3/script_01.js
@@ -1,23 +1,17 @@
 // Dada uma matriz de matrizes, transforme em uma única matriz.
 
-
 const assert = require('assert');
 
-const arrays = [
-    ["1", "2", "3"],
-    [true],
-    [4, 5, 6]
-];
-
+const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
 
 function flatten() {
   // escreva seu código aqui
 
-  const flat = arrays.reduce( (acc, array) => {
-    return acc.concat(array)
-  })
+  const flat = arrays.reduce((acc, array) => {
+    return acc.concat(array);
+  });
 
-  return flat
+  return flat;
 }
 
-assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
+assert.deepStrictEqual(flatten(), ['1', '2', '3', true, 4, 5, 6]);
