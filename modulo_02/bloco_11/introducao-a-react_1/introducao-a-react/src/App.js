628f7990796c350f8195a19e99d943d57810f579
Matheus Martino <matheus.martino97@gmail.com>
2021-02-11 20:30:39 -0300

    Teste.

--- modulo_02/bloco_11/introducao-a-react_1/introducao-a-react/src/App.js
+++ modulo_02/bloco_11/introducao-a-react_1/introducao-a-react/src/App.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './App.css';
 
 const tarefas = ['Acordar', 'Tomar café', 'Escovar os dentes', 'Ir trabalhar']
 
+
+class Texto extends Component {
+  render() {
+    return (
+    <div>
+      <h1>Matheus</h1>
+      <p>Teste</p>
+    </div>
+    );
+  }
+}
+
 function App() {
   return (
-    <ul>{ tarefas.map(tarefa => <li>{ tarefa }</li>) }</ul>
+  <div>
+    <Texto />
+  </div>
   );
 }
 
