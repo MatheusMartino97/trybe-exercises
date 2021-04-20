04f0ebf5a3488bbbb6ede76ddcc7e28018817965
Matheus Martino <matheus.martino97@gmail.com>
2021-04-20 18:21:09 -0300

    Adding hooks.

--- modulo_02/bloco_18/context-api-e-react-hooks_3/src/contexts/ToDoListContext.js
+++ modulo_02/bloco_18/context-api-e-react-hooks_3/src/contexts/ToDoListContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext } from 'react';
 import useArray from '../hooks/useArray'
 
 export const ToDoListContext = createContext();
