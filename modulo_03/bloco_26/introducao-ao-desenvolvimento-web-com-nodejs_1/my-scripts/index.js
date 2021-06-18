97ed27e24b064079902a9388d24e3455fff6eadc
Matheus Martino <matheus.martino97@gmail.com>
2021-06-17 21:26:00 -0300

    Mudando os scripts pra dentro da pasta scripts

--- modulo_03/bloco_26/introducao-ao-desenvolvimento-web-com-nodejs_1/my-scripts/index.js
+++ modulo_03/bloco_26/introducao-ao-desenvolvimento-web-com-nodejs_1/my-scripts/index.js
@@ -1,8 +1,8 @@
 const { questionInt, question } = require('readline-sync');
 
-const imc = require('./imc');
-const velocidade = require('./velocidade');
-const sorteio = require('./sorteio');
+const imc = require('./scripts/imc');
+const velocidade = require('./scripts/velocidade');
+const sorteio = require('./scripts/sorteio');
 
 const scripts = {
   1: 'imc',
@@ -14,33 +14,35 @@ function chooseScript() {
   console.log(scripts);
   const script = questionInt('Escolha um script');
 
-  runScript(script)
+  runScript(script);
 }
 
 function runScript(script) {
   switch (script) {
     case 1:
+      console.log(`Você escolheu o script ${scripts[script].toUpperCase()}`);
       imc();
       break;
 
     case 2:
+      console.log(`Você escolheu o script ${scripts[script].toUpperCase()}`);
       velocidade();
       break;
 
     default:
+      console.log(`Você escolheu o script ${scripts[script].toUpperCase()}`);
       sorteio();
       break;
   }
 
-  askPlayAgain()
+  askPlayAgain();
 }
-
 
 function askPlayAgain() {
   const playAgain = question('Quer rodar outro sript?');
-  
+
   if (playAgain === 'sim') {
-    chooseScript()
+    chooseScript();
   }
 }
 
