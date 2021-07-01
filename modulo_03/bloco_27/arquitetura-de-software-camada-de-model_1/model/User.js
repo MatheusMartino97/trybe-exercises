189e44436fbab4b9abbfe60bac9c89efb46a1e7d
Matheus Martino <matheus.martino97@gmail.com>
2021-07-01 18:36:24 -0300

    Exercicios.

---
+++ modulo_03/bloco_27/arquitetura-de-software-camada-de-model_1/model/User.js
@@ -0,0 +1,42 @@
+const connection = require('./connection');
+const { ObjectId } = require('mongodb');
+
+const getAll = () => {
+  return connection()
+    .then((db) => db.collection('users').find().toArray())
+    .then((result) =>
+      result.map(({ _id, firstName, lastName, email }) => ({
+        id: _id,
+        firstName,
+        lastName,
+        email,
+      }))
+    );
+};
+
+const getById = (id) => {
+  return connection()
+    .then((db) => db.collection('users').find(new ObjectId(id)).toArray())
+    .then((result) => result);
+};
+
+const create = (firstName, lastName, email, password) => {
+  return connection()
+    .then((db) =>
+      db.collection('users').insertOne({ firstName, lastName, email, password })
+    )
+    .then((result) => ({
+      id: result.insertedId,
+      firstName,
+      lastName,
+      email,
+      password,
+    }))
+    .catch((err) => console.log(err.message));
+};
+
+module.exports = {
+  create,
+  getAll,
+  getById,
+};
